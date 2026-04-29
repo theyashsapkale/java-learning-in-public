aa1de5b0b27f3e2f6a9b17f800bdea3d53040fbf
theyashsapkale <theyashsapkale@gmail.com>
2026-04-29 18:06:47 +0530

    ArrayPairSUm que

---
+++ ArrayPairSum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int n = nums.length;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0 ; j < n - i - 1; j++ ){
+                if(nums[j+1]< nums[j]){
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+
+        int sum = 0 ;
+        for(int i = 0; i < n ; i+=2){
+            sum += nums[i];
+        }
+         return sum ; 
+    }
+}
