d83c10c2d5bfcab40c1eb2e68196d2964887ff72
theyashsapkale <theyashsapkale@gmail.com>
2026-05-03 16:31:00 +0530

    preFix sum of array

---
+++ PrefixArr.java
@@ -0,0 +1,30 @@
+
+public class PrefixArr {
+
+    public static int[] preF(int arr[]){
+
+        int n = arr.length;
+
+        int temp[]= new int[n];
+
+           temp[0]= arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+           temp[i] = temp[i-1]+arr[i];
+        }
+        return temp;
+    }
+
+     public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+
+        int temp[] = preF(arr);
+
+        for (int i = 0; i < temp.length; i++) {
+            System.out.println(temp[i]);
+        }
+
+     }
+} {
+    
+}
