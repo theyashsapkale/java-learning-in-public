a4ef8b0598edb739895e219110617d93483831b5
theyashsapkale <theyashsapkale@gmail.com>
2026-05-08 23:51:30 +0530

    [200~

---
+++ PalindromeNumber.java
@@ -0,0 +1,30 @@
+public class PalindromeNumber {
+     static  boolean Palindrome(int x){
+       
+        if (x < 0 ) {
+            return false ;
+        }
+
+        int n = x;
+
+        int revNum = 0;
+
+        while(n>0){
+            int d = n% 10;
+            revNum = revNum * 10 + d;
+            n = n / 10;
+        }
+
+        if (revNum == x ) {
+            return true;
+        }else{
+            return false ;
+        }
+
+     }
+    public static void main(String[] args) {
+        int x = 151;
+        boolean result = Palindrome(x);
+        System.out.println(result);
+    }
+}
